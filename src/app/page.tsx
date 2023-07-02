06421162403c498a915a48ab4c6ec3712e1037a0
Alkesh Gandhi <alkeshghandhi@Ks-MacBook-Pro.local>
2023-07-02 14:09:17 +0800

    Changed message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
             Deploy <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Jaini Gandhi
           </p>
         </a>
       </div>
